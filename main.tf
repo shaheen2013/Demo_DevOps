9f08dfecc3af16604e57f1b05804e3177e86e1de
shaheen2013 <shaheenmediusware@gmail.com>
2025-05-28 15:19:45 +0600

    new bucket added16

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "terraform_state1" {
-  bucket = "my-terraform-bucket-for-demo1"  # Replace with a globally unique name
+  provider = aws
+  bucket = "my-terraform-bucket-for-demo3"  # Replace with a globally unique name
   tags = {
     Name        = "Terraform Bucket"
     Environment = "demo"
@@ -7,9 +8,31 @@ resource "aws_s3_bucket" "terraform_state1" {
 }
 
 resource "aws_s3_bucket" "terraform_state2" {
-  bucket = "my-terraform-bucket-for-demo2"  # Replace with a globally unique name
+  provider = aws
+  bucket = "my-terraform-bucket-for-demo4"  # Replace with a globally unique name
   tags = {
     Name        = "Terraform Bucket"
     Environment = "demo"
   }
 }
+
+# # ---- Azure: Create Resource Group ----
+# resource "azurerm_resource_group" "azure_rg" {
+#   provider = azurerm
+#   name     = "rg-multicloud"
+#   location = "East US"
+# }
+
+# # ---- Azure: Create Storage Account ----
+# resource "azurerm_storage_account" "azure_storage" {
+#   provider                = azurerm
+#   name                    = "mystorageacct12345" # must be unique, lowercase only
+#   resource_group_name     = azurerm_resource_group.azure_rg.name
+#   location                = azurerm_resource_group.azure_rg.location
+#   account_tier            = "Standard"
+#   account_replication_type = "LRS"
+
+#   tags = {
+#     Environment = "Multi-Cloud"
+#   }
+# }
