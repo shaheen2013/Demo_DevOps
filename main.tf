5ead8ace6058669d52140c8d816fe0e00a608077
shaheen2013 <shaheenmediusware@gmail.com>
2025-05-28 18:21:52 +0600

    new bucket

--- main.tf
+++ main.tf
@@ -1,17 +1,7 @@
-resource "aws_s3_bucket" "my_bucket1" {
+resource "aws_s3_bucket" "my_bucket" {
   provider = aws
   bucket = "my-terraform-bucket-for-demo1"  # Replace with a globally unique name
   tags = {
-    Name        = "Terraform Bucket"
-    Environment = "demo"
-  }
-}
-
-resource "aws_s3_bucket" "my_bucket2" {
-  provider = aws
-  bucket = "my-terraform-bucket-for-demo2"  # Replace with a globally unique name
-  tags = {
-    Name        = "Terraform Bucket"
-    Environment = "demo"
+    Name = "Terraform Bucket"
   }
 }
--- provider.tf
+++ provider.tf
@@ -1,9 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-
-# provider "azurerm" {
-#   alias = "azure"
-#   features {}
-# }
