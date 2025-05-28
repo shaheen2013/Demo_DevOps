26a595a0e72ae059caf07f7b448f4d9679d46260
shaheen2013 <shaheenmediusware@gmail.com>
2025-05-28 18:34:28 +0600

    new bucket demo3

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "my_bucket" {
   provider = aws
-  bucket = "my-terraform-bucket-for-demo2"  # Replace with a globally unique name
+  bucket = "my-terraform-bucket-for-demo3"  # Replace with a globally unique name
   tags = {
     Name = "Terraform Bucket"
   }
