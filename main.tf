f1ca288c29d95630829a5774846e6739e3d12605
shaheen2013 <shaheenmediusware@gmail.com>
2025-05-28 15:08:07 +0600

    new bucket added16

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_s3_bucket" "terraform_state" {
     Environment = "demo"
   }
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-bucket-for-demo15"  # Replace with a globally unique name
+  tags = {
+    Name        = "Terraform Bucket"
+    Environment = "demo"
+  }
+}
