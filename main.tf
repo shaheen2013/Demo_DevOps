2a3adf718e1c6eb59fc761caa179bcb4e237a184
shaheen2013 <shaheenmediusware@gmail.com>
2025-05-28 16:26:20 +0600

    new bucket

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "terraform_state1" {
+resource "aws_s3_bucket" "my_bucket1" {
   provider = aws
   bucket = "my-terraform-bucket-for-demo1"  # Replace with a globally unique name
   tags = {
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "terraform_state1" {
   }
 }
 
-resource "aws_s3_bucket" "terraform_state2" {
+resource "aws_s3_bucket" "my_bucket2" {
   provider = aws
   bucket = "my-terraform-bucket-for-demo2"  # Replace with a globally unique name
   tags = {
@@ -15,24 +15,3 @@ resource "aws_s3_bucket" "terraform_state2" {
     Environment = "demo"
   }
 }
-
-# # ---- Azure: Create Resource Group ----
-# resource "azurerm_resource_group" "azure_rg" {
-#   provider = azurerm
-#   name     = "rg-multicloud"
-#   location = "East US"
-# }
-
-# # ---- Azure: Create Storage Account ----
-# resource "azurerm_storage_account" "azure_storage" {
-#   provider                = azurerm
-#   name                    = "mystorageacct12345" # must be unique, lowercase only
-#   resource_group_name     = azurerm_resource_group.azure_rg.name
-#   location                = azurerm_resource_group.azure_rg.location
-#   account_tier            = "Standard"
-#   account_replication_type = "LRS"
-
-#   tags = {
-#     Environment = "Multi-Cloud"
-#   }
-# }
--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+
 # provider "azurerm" {
 #   alias = "azure"
 #   features {}
